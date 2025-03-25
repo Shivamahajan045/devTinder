a992ffb54178889796bd4909d663d3401ade92fc
shivakumar <shivamahajan055@gmail.com>
2025-03-25 15:57:53 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -1,34 +1,35 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-  res.send({
-    userId: req.params.userId,
-    firstName: "Shiva",
-    lastName: "Mahajan",
-  });
-});
+//app.use("/user", [rh1, rh2, rh3, rh4,.....])
+//app.use("/user", [rh1, rh2], rh3, rh4, ....)
 
-app.get("/user", (req, res) => {
-  console.log(req.query);
-  res.send({
-    firstName: "Shiva",
-    lastName: "Mahajan",
-  });
-});
-
-app.post("/user", (req, res) => {
-  res.send("User created successfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("Deleted successfully!");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from the server");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling user1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling user2");
+    // res.send("Response 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling user3");
+    // res.send("Response 3");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling user4");
+    // res.send("Response 4");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling user5");
+    res.send("Response 4");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Server is successfully listening to 7777");
