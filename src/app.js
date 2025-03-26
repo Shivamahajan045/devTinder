71d66abe8b05225f6274e0f5e923d838098e30f7
shivakumar <shivamahajan055@gmail.com>
2025-03-26 16:17:26 +0530

    writing-auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,35 +1,24 @@
 const express = require("express");
 const app = express();
+const { adminAuth, userAuth } = require("../middlewares/auth");
+// Middlewares why do we need it
 
-//app.use("/user", [rh1, rh2, rh3, rh4,.....])
-//app.use("/user", [rh1, rh2], rh3, rh4, ....)
+app.use("/admin", adminAuth);
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("Handling user1");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling user2");
-    // res.send("Response 2");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling user3");
-    // res.send("Response 3");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling user4");
-    // res.send("Response 4");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling user5");
-    res.send("Response 4");
-  }
-);
+app.get("/user/login", (req, res) => {
+  res.send("user data sent!");
+});
+
+app.get("/user/data", userAuth, (req, res) => {
+  res.send("user data sent!");
+});
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data sent!");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("Deleted user!");
+});
 
 app.listen(7777, () => {
   console.log("Server is successfully listening to 7777");
