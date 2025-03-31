c1e07bd4f27af2e7b4f9a6146eb436fb0d1f1969
shivakumar <shivamahajan055@gmail.com>
2025-03-31 16:28:55 +0530

    initializing-database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
+
+//signup
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Mahendrasingh",
+    lastName: "Dhoni",
+    emailId: "msd@gmail.com",
+    password: "Msd@123",
+    age: 47,
+  });
+  try {
+    await user.save();
+    res.status(201).send("User created successfully");
+  } catch (err) {
+    res.status(400).send("User not created", err.message);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("Database connnected successfully...");
 
     app.listen(7777, () => {
-      console.log("Server is successfully listening to 7777");
+      console.log("Server is successfully listening to 7777....");
     });
   })
   .catch((err) => {
