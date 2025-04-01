6241b8186d7130d76bb62fea91340844ccb8e5cc
shivakumar <shivamahajan055@gmail.com>
2025-04-01 15:40:35 +0530

    get request

--- src/app.js
+++ src/app.js
@@ -7,13 +7,58 @@ app.use(express.json());
 
 //signup
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
   const user = new User(req.body);
   try {
     await user.save();
     res.status(201).send("User created successfully");
   } catch (err) {
     res.status(400).send("User not created", err.message);
+  }
+});
+
+//get user - using Model.find() - return a array of objects
+// app.get("/user", async (req, res) => {
+//   let userEmail = req.body.emailId;
+//   try {
+//     let user = await User.find({ emailId: userEmail });
+//     console.log(user);
+//     if (user.length === 0) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.status(201).send(user);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
+
+// Model.find({}) - empty filter will return all documents from the model
+// app.get("/user", async (req, res) => {
+//   try {
+//     let user = await User.find({});
+//     console.log(user);
+//     if (user.length === 0) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.status(201).send(user);
+//     }
+//   } catch (err) {
+//     res.status(400).send("Something went wrong");
+//   }
+// });
+
+//Model.findOne() - return a 1st document if filter matches the multiple documents
+app.get("/user", async (req, res) => {
+  let userEmail = req.body.emailId;
+  try {
+    let user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.status(201).send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
